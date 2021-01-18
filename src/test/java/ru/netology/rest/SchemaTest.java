cc15a4fa585c4784b25132980209267a9bc1be90
BethPech <colnze17-94@mail.ru>
2021-01-19 01:33:29 +0300

    Returnet to Rub

--- src/test/java/ru/netology/rest/SchemaTest.java
+++ src/test/java/ru/netology/rest/SchemaTest.java
@@ -17,7 +17,7 @@ public class SchemaTest {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .body("[0].currency", equalTo("RUR"))
+                .body("[0].currency", equalTo("RUB"))
         ;
     }
 }
