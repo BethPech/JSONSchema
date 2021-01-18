d5f45b9a7264739528e81e4c684a0a357ccd013d
BethPech <colnze17-94@mail.ru>
2021-01-18 16:29:10 +0300

    Tets failed with 200

--- src/test/java/ru/netology/rest/SchemaTest.java
+++ src/test/java/ru/netology/rest/SchemaTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 public class SchemaTest {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldRunTest() {
         given()
                 .baseUri("http://json-schema.org/draft-07/schema")
                 .when()
@@ -15,7 +16,6 @@ public class SchemaTest {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-
         ;
     }
 }
